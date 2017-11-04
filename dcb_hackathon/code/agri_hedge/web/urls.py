8b207ce0301c939d875b0209f1f2673214e33282
sudmit <sudhanshu@attivonetworks.com>
2017-11-05 04:49:29 +0530

    Completed the code for fetching mandi data

--- dcb_hackathon/code/agri_hedge/web/urls.py
+++ dcb_hackathon/code/agri_hedge/web/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='Home'),
+    url(r'^fetch_prices$', views.fetch_current_price, name='fetch_prices'),
     #url(r'all_ossec_rules$', views.all_ossec_rules, name='all_ossec_rules'),
 ]
 
--- dcb_hackathon/code/agri_hedge/web/views.py
+++ dcb_hackathon/code/agri_hedge/web/views.py
@@ -20,6 +20,8 @@ import json
 import time
 import string
 import datetime
+import traceback
+import requests
 
 # Create your views here.
 #@login_required
@@ -42,23 +44,27 @@ def fetch_current_price(request):
 			if status_request != "":
 				status_request = status_request.json()
 				for record in status_request['records']:
-					final_list[str(count)] = {}
+					str_count = str(count)
+					final_list[str_count] = {}
 					for entry in record:
-						final_list[str(count)][entry] = record[entry]
-					state = final_list[str(count)]['state']
-					district = final_list[str(count)]['district']
-					market = final_list[str(count)]['market']
-					commodity = final_list[str(count)]['commodity']
-					variety = final_list[str(count)]['variety']
-					min_price = final_list[str(count)]['min_price']
-					max_price = final_list[str(count)]['max_price']
-					modal_price = final_list[str(count)]['modal_price']
-					arrival_date = final_list[str(count)]['arrival_date']
-					updated_at = timezone.now
+						final_list[str_count][entry] = record[entry]
+					state = final_list[str_count]['state']
+					district = final_list[str_count]['district']
+					market = final_list[str_count]['market']
+					commodity = final_list[str_count]['commodity']
+					variety = final_list[str_count]['variety']
+					min_price = final_list[str_count]['min_price']
+					max_price = final_list[str_count]['max_price']
+					modal_price = final_list[str_count]['modal_price']
+					arrival_date = final_list[str_count]['arrival_date']
+					updated_at = str(timezone.now())
+					print state, district, market, commodity, variety, min_price, max_price, modal_price, arrival_date, updated_at
 					
-					final_list[str(count)]['composite_cp_id'] = state.lower()+'_'+district.lower()+'_'+market.lower()+'_'+commodity.lower()+'_'+variety.lower()
+					final_list[str_count]['composite_cp_id'] = (state+'_'+district+'_'+market+'_'+commodity+'_'+variety).lower()
 
-					(page_data_save, page_data_save_status)= models.current_price_table.objects.update_or_create(composite_cp_id=str(final_list[str(count)]['composite_cp_id']), defaults={'commodity': commodity,'variety': variety,'state': state,'district': district,'market': market,'min_price': min_price,'max_price': max_price,'modal_price': modal_price,'arrival_date': arrival_date,'updated_at': updated_at})
+					(page_data_save, page_data_save_status)= models.current_price_table.objects.update_or_create(composite_cp_id=str(final_list[str_count]['composite_cp_id']), defaults={'commodity': commodity,'variety': variety,'state': state,'district': district,'market': market,'min_price': min_price,'max_price': max_price,'modal_price': modal_price,'arrival_date': arrival_date,'updated_at': updated_at})
+					#Add page_data_save_status to a log file
+					print page_data_save_status
 					count += 1
 			http_response_json['status'] = True
 			http_response_json['message'] = final_list
@@ -66,7 +72,9 @@ def fetch_current_price(request):
 			http_response_json['status'] = False
 			http_response_json['message'] = "Response is empty OR status code is not ok."
 	except Exception as err:
+		print traceback.print_exc()
+		print err
 		http_response_json['status'] = False
 		http_response_json['message'] = "Exception occured: %s"%(err)
 	finally:
-		return HttpResponse(http_response_json, content_type='application/json')
+		return HttpResponse(json.dumps(http_response_json), content_type='application/json')
